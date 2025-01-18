4772ed79b754d2206e4cee30049e17095914c6af
Arthur Anthony <arthuranthony2000@gmail.com>
2025-01-18 15:03:57 -0300

    adjust project

--- config/db.php
+++ config/db.php
@@ -5,8 +5,8 @@ return [
     'dsn' => 'mysql:host=localhost:3306;dbname=sistema_financeiro',
     'username' => 'root',
     'password' => 'RrR@09022000',
-    // 'password' => '',
-    //'password' => 'RrR.@123',
+    // 'password' => '', 
+    //'password' => 'RrR.@123', 
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
