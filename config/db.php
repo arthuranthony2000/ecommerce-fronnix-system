3e009ae5b221e9316f6353dba5da3a52d72e9bb1
Arthur Anthony <arthuranthony2000@gmail.com>
2025-01-18 15:03:22 -0300

    insert final dissertation version

--- config/db.php
+++ config/db.php
@@ -4,7 +4,9 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost:3306;dbname=sistema_financeiro',
     'username' => 'root',
-    'password' => 'RrR.@123',
+    'password' => 'RrR@09022000',
+    // 'password' => '',
+    //'password' => 'RrR.@123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
